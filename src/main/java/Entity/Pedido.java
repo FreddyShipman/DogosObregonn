b51d8572cb781acba246fb613dcdd8c511667822
FreddyShipman <jose.guzman252524@potros.itson.edu.mx>
2025-10-08 20:01:44 -0700

    Entidad pedido terminada

--- src/main/java/Entity/Pedido.java
+++ src/main/java/Entity/Pedido.java
@@ -1,10 +1,20 @@
 package Entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Set;
+import static org.hibernate.engine.internal.Cascade.cascade;
 
 /**
  *
@@ -17,5 +27,68 @@ public class Pedido implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+    private LocalDateTime fecha;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "metodo_pago", nullable = false)
+    private MetodoPago metodoPago;
+    
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.REMOVE)
+    private Set<PedidoDetalle> detalles;
 
+    public Pedido() {
+    }
+
+    public Pedido(Long id, Cliente cliente, LocalDateTime fecha, MetodoPago metodoPago, Set<PedidoDetalle> detalles) {
+        this.id = id;
+        this.cliente = cliente;
+        this.fecha = fecha;
+        this.metodoPago = metodoPago;
+        this.detalles = detalles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDateTime fecha) {
+        this.fecha = fecha;
+    }
+
+    public MetodoPago getMetodoPago() {
+        return metodoPago;
+    }
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public Set<PedidoDetalle> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(Set<PedidoDetalle> detalles) {
+        this.detalles = detalles;
+    }
+    
+    
 }

2b734eaa92f4073f11d8d80b79353eaeb39528b6
FreddyShipman <jose.guzman252524@potros.itson.edu.mx>
2025-10-08 19:46:44 -0700

    Creacion del proyecto y conexiones hechas

---
+++ src/main/java/dogosobregonjpa/dogosobregon/DogosObregon.java
@@ -0,0 +1,12 @@
+package dogosobregonjpa.dogosobregon;
+
+/**
+ *
+ * @author alfre
+ */
+public class DogosObregon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

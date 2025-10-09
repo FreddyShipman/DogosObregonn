086b422251adb5bf97f7157ca2b3101fe06b64dc
FreddyShipman <jose.guzman252524@potros.itson.edu.mx>
2025-10-08 19:54:01 -0700

    Entidad HotDog hecha

--- src/main/java/Entity/HotDog.java
+++ src/main/java/Entity/HotDog.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Set;
 
 /**
  *
@@ -17,5 +20,63 @@ public class HotDog implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String nombre;
+    private BigDecimal precio;
+    private BigDecimal iva;
+    
+    @OneToMany(mappedBy = "hotdog")
+    private Set<PedidoDetalle> detalles;
+
+    public HotDog() {
+    }
+
+    public HotDog(Long id, String nombre, BigDecimal precio, BigDecimal iva, Set<PedidoDetalle> detalles) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.iva = iva;
+        this.detalles = detalles;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public BigDecimal getIva() {
+        return iva;
+    }
+
+    public void setIva(BigDecimal iva) {
+        this.iva = iva;
+    }
+
+    public Set<PedidoDetalle> getDetalles() {
+        return detalles;
+    }
+
+    public void setDetalles(Set<PedidoDetalle> detalles) {
+        this.detalles = detalles;
+    }
+    
     
 }
